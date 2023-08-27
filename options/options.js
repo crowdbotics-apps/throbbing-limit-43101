b9409f90c2d923e7c5a5f7c679b31f6acc6440b8
Crowdbotics <team@crowdbotics.com>
2023-08-27 11:05:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "throbbing-limit-43101",
+  url: "https://throbbing-limit-43101.botics.co",
+  api: "https://throbbing-limit-43101.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
